5d1c157321e7b10d9e878f732c7f5e7511e701d5
Flynn Buckingham <dev@flynnbuckingham.com>
2019-07-12 23:59:43 -0400

    Init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# WSL 2 Script Collection
+
+A (WIP) small collection of bash scripts to ease development tasks within a WSL 2 environment.
+
+[A shell include for exporting your WSL and host IP, with a small echo](./wsl-rc-env-helper.sh)
---
+++ wsl-rc-env-helper.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Minimal helper script for easily displaying relevant environment
+# information for local web development in a WSL 2.
+
+# Intended to be included within the your ~/.bashrc or ~/.zshrc
+
+# Get the primary IP address of WSL 2 distro (guest IP)
+wsl_address="$(hostname -I | xargs)";
+
+# Get the address of the Windows 10 host IP
+host_address="$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}')";
+
+# Ensure the values are additionally exported for reuse in the env script
+export WSL_IP=$wsl_address;
+export WSL_HOST=$host_address;
+
+echo "$(uname -a)
+
+This instance has been assigned $wsl_address (\$WSL_IP)
+The Windows Host machine $(hostname) can be accessed from $host_address (\$WSL_HOST)
+
+";
